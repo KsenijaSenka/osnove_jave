2fa411fdbba9bfa5a7b5320de689e72c3669e359
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2023-08-20 14:59:27 +0200

    Add solution

--- src/main/java/p18_08_2023/Zadatak10.java
+++ src/main/java/p18_08_2023/Zadatak10.java
@@ -4,6 +4,34 @@ package p18_08_2023;
 //        Za neaktivnu pozciju stampa _
 //        Korisnik unosi granice za nekativni opseg, unoseci A i B.
 
-public class Zadatak10 {
+import java.util.Scanner;
 
+public class Zadatak10 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Unesite a: ");
+        int a = s.nextInt();
+        System.out.print("Unesite b: ");
+        int b = s.nextInt();
+
+
+        for (int i = 1; i < 21; i++) {
+
+            if (i >= a && i <= b) {
+                System.out.print("_");
+            } else {
+                System.out.print("*");
+            }
+        }
+    }
 }
+
+
+
+
+
+
+            
+
+
+
