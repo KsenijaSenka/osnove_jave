1703957433658b7e7b13a0b34c9a92056fd8cad9
Ksenija <ksenijabeocanin@gmail>
2023-08-27 16:10:41 +0200

    Add Assignments 1-7

--- src/main/java/d25_08_2023/Zadatak5.java
+++ src/main/java/d25_08_2023/Zadatak5.java
@@ -1,2 +1,23 @@
-package d25_08_2023;public class Zadatak5 {
+package d25_08_2023;
+
+import java.util.Scanner;
+//Napisati funkciju koja vrsi konverziju eura u dinare, rublje ili dolare prema dole navedenoj konverziji. Metoda od parametara prima vrednost u eurima kao i valutu u kojoj je potrebno izvrsiti konverziju (RSD/USD/RUB)
+     //   a vraca konvertovanu vrednost.
+public class Zadatak5 {
+    public static void main(String[] args) {
+
+        double konvertovanaVrednost = konvertuj(10, "USD");
+        System.out.println(konvertovanaVrednost);
+    }
+
+    public static double konvertuj(double vrednost, String valuta) {
+        if (valuta.equals("RSD")) {
+            return vrednost * 117.5;
+        } else if (valuta.equals("USD")) {
+            return vrednost * 1.1;
+        } else if (valuta.equals("RUB")) {
+            return vrednost * 62.98;
+        } else return 0;
+    }
 }
+
--- src/main/java/d25_08_2023/Zadatak6.java
+++ src/main/java/d25_08_2023/Zadatak6.java
@@ -1,2 +1,24 @@
-package d25_08_2023;public class Zadatak6 {
+package d25_08_2023;
+
+import java.util.Scanner;
+
+//Napisati funkciju koja pronalazi
+// koliko ima celih brojeve izmedju brojeva M i N.
+public class Zadatak6 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unestite 2 broja");
+        int x1 = s.nextInt();
+        int x2 = s.nextInt();
+        int brojCelih = izbroj(x1, x2);
+        System.out.println("Između unetih brojeva ima " + brojCelih +
+                " celih brojeva ");
+
+        //for (int i = x1 + 1; i < x2; i++) {
+           // System.out.print(i + ", ");}
+    }
+
+    public static int izbroj(int m, int n) {
+        return n - m - 1;
+    }
 }
